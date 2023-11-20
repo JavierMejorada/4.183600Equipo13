514baa4911b7786fc91191293bd1454867a7fdac
HP <HP@DESKTOP-T3JDKAG>
2023-11-20 14:01:56 -0600

    carga inicial

---
+++ src/main/java/com/mycompany/equipo13/App.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.equipo13;
+
+/**
+ *
+ * Mejorada Soto Jose Javier
+ * Vallejo Ramirez Emmanuel
+ */
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("PROYECTO TEMA NO.4");
+    }
+    public static int MENU_PRINCIPAL(){
+        int op=0;
+        
+        
+        
+        return op;
+    }
+}
